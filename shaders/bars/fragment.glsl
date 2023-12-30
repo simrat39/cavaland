cd39d2030e987c8139a4baf91c6e7d4e9c8585ea
Simrat Grewal <simrats169169@gmail.com>
2023-12-29 21:15:43 -0800

    Shader gradients and Update screenshot

--- shaders/bars/fragment.glsl
+++ shaders/bars/fragment.glsl
@@ -5,8 +5,11 @@ in vec4 col;
 out vec4 FragColor;
 
 void main() {
-  vec3 color1 = vec3(0.305f, 0.329f, 0.784f);
-  vec3 color2 = vec3(0.56f, 0.580f, .984f);
+  vec3 color1 = vec3(69.f, 104.f, 220.f);
+  vec3 color2 = vec3(176.f, 106.f, 179.f);
+  vec3 normalize = vec3(255.f);
 
-  FragColor = vec4(mix(color1, color2, abs((col.x + 1.0f) / 2.0f)), 1.f);
+  FragColor = vec4(mix(color1 / normalize, color2 / normalize, gl_FragCoord.y /
+  (1920.f / 20.f)), 1.f);
+   // FragColor = vec4();
 }
